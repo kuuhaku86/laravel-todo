a1fe4fcb708fcc907625b8f53354a7aa870eed5d
kuuhaku86 <yohan.ardiansyah90@gmail.com>
2021-04-06 13:23:41 +0700

    finishing the website

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TasksController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::get('/dashboard',[TasksController::class, 'index'])->name('dashboard');
+
+    Route::get('/task',[TasksController::class, 'add']);
+    Route::post('/task',[TasksController::class, 'create']);
+
+    Route::get('/task/{task}', [TasksController::class, 'edit']);
+    Route::post('/task/{task}', [TasksController::class, 'update']);
 });
